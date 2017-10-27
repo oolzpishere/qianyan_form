23106c593ffd026f8f69f5514884b99681f4a29a
leeli <leeli@leelis-iMac.local>
2017-10-28 00:12:45 +0800

    change lib post

--- lib/xls/post.rb
+++ lib/xls/post.rb
@@ -3,23 +3,23 @@
 module Xls
   class Post
 
-    attr_reader :file, :file_path,:hashs, :uri, :pass, :subject
-    
-    # params need: file, url, subject(CamelCase class)
+    attr_reader :file_path,:hashs, :uri, :pass, :subject
+
+    # params: { file, url, subject(class name with CamelCase string)}
     def initialize(ps = {})
       @file_path = ps[:file] || File.join(File.expand_path("../files", __FILE__), "_v9KUmP_小学英语会议报名_20170817162821.xls")
       @hashs = Xls::Parse.new(file: file_path).to_hash
       url = ps[:url] || "http://localhost:3000/results"
-      @uri =  gen_uri(url)
       @pass = ENV["QIANYAN_FORM_PASS"]
       @subject = ps[:subject]
+      @uri =  gen_uri(url)
     end
-    
+
     def post(range = 0..-1)
       # rel_range = Range.new(range[0], range[1])
       ranged_hashs = hashs[range]
       ranged_hashs.each do |row|
-        req = Net::HTTP::Post.new( uri.path, initheader = {"Content-Type" => "application/json", 'Accept' => 'application/json'} ) 
+        req = Net::HTTP::Post.new( uri.path, initheader = {"Content-Type" => "application/json", 'Accept' => 'application/json'} )
         req.body =  row.merge({'pass' => pass, 'subject' => subject}).to_json
         res = Net::HTTP.start(uri.hostname, uri.port) do |http|
           http.request(req)
