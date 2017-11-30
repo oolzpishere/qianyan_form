6b111e310a0424a03fdd66f45e6b0f117430539b
leeli <leeli@leelis-iMac.local>
2017-11-30 16:44:40 +0800

    change deploy to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/oolzpishere/qianyan_form.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, "v1.01"
+# set :branch, "v1.01"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/var/www/rails/#{fetch(:application)}"
