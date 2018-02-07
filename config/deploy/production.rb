df0aeda17dbc36273b76368db3e62396e689db08
leeli <leeli@leelis-iMac.local>
2018-02-07 12:54:38 +0800

    try to remove cap rbenv version setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -72,9 +72,10 @@ server "qianyan.sflx.com.cn",
        }
 
 # rbenv setting
-set :rbenv_ruby, '2.4.1'
+# set :rbenv_ruby, '2.4.1'
 # set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_prefix, "source ~/.zshrc; RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "source ~/.zshrc; RBENV_ROOT=#{fetch(:rbenv_path)}  #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_prefix, "source ~/.zshrc; RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
