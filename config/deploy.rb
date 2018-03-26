e237a81b2ba6a9d51fe0c6873e309f8c4b99f6cf
leeli <leeli@leelis-iMac-2.local>
2018-03-26 13:37:05 +0800

    update deploy lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock "3.9.0"
+lock "3.10.1"
 
 set :application, "qianyan_form"
 set :repo_url, "https://github.com/oolzpishere/qianyan_form.git"
